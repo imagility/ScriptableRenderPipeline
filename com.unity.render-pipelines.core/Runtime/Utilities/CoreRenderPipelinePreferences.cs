54701ddf0e598c47937b22409fc68e7bf1129cfd
Thomas <chman.a11w@gmail.com>
2019-02-12 17:21:43 +0100

    Volume color/alpha customization

---
+++ com.unity.render-pipelines.core/Runtime/Utilities/CoreRenderPipelinePreferences.cs
@@ -0,0 +1,67 @@
+namespace UnityEngine.Rendering
+{
+    // This file can't be in the editor assembly as we need to access it in runtime-editor-specific
+    // places like OnGizmo etc and we don't want to add the editor assembly as a dependency of the
+    // runtime one
+
+#if UNITY_EDITOR
+    using UnityEditor;
+
+    public static class CoreRenderPipelinePreferences
+    {
+        static bool m_Loaded = false;
+
+        static Color s_VolumeGizmoColor = new Color(0.2f, 0.8f, 0.1f, 0.5f);
+        public static Color volumeGizmoColor
+        {
+            get => s_VolumeGizmoColor;
+            set
+            {
+                if (s_VolumeGizmoColor == value) return;
+                s_VolumeGizmoColor = value;
+                EditorPrefs.SetInt(Keys.volumeGizmoColor, (int)ColorUtils.ToHex(value));
+            }
+        }
+
+        static class Keys
+        {
+            internal const string volumeGizmoColor = "CoreRP.Volume.GizmoColor";
+        }
+
+        [SettingsProvider]
+        static SettingsProvider PreferenceGUI()
+        {
+            return new SettingsProvider("Preferences/Core Render Pipeline", SettingsScope.User)
+            {
+                guiHandler = searchContext =>
+                {
+                    if (!m_Loaded)
+                        Load();
+
+                    EditorGUILayout.Space();
+
+                    volumeGizmoColor = EditorGUILayout.ColorField("Volume Gizmo Color", volumeGizmoColor);
+                }
+            };
+        }
+
+        static CoreRenderPipelinePreferences()
+        {
+            Load();
+        }
+
+        static void Load()
+        {
+            s_VolumeGizmoColor = GetColor(Keys.volumeGizmoColor, new Color(0.2f, 0.8f, 0.1f, 0.5f));
+
+            m_Loaded = true;
+        }
+
+        static Color GetColor(string key, Color defaultValue)
+        {
+            int value = EditorPrefs.GetInt(key, (int)ColorUtils.ToHex(defaultValue));
+            return ColorUtils.ToRGBA((uint)value);
+        }
+    }
+#endif
+}
